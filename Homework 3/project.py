35f4cb207a08d45732e2aff5c8c99f7d725f8f4b
garthBates <garth.bates@wsu.edu>
2022-03-25 12:19:56 -0700

    Added populateMessageList function

--- Homework 3/project.py
+++ Homework 3/project.py
@@ -5,13 +5,29 @@
 # Date Completed: 4/5/2022
 ##############################
 
+stopWords = "../Homework 3/fortune-cookie-data/stoplist.txt"
 trainingCookieMessages = "../Homework 3/fortune-cookie-data/traindata.txt"
 trainingCookieLabels = "../Homework 3/fortune-cookie-data/trainlabels/txt"
+
+trainingSetsDict = {}       #{phrase: label}
+messageList = []            #A list of all the messages. Used to clean messages before populating the trainingSetDict
+
+############################################################ Pre-Processing ##########################################################
+def populateMessageList(inputFile):
+    fileRead = open(inputFile, 'r')
+    messages = fileRead.readlines()
+    fileRead.close()
+
+    return messages
+    
+
 
 ################################################################ Main ################################################################
 
 def main():
-    return 0
+    messageList = populateMessageList(trainingCookieMessages)
+
+    print(messageList)
 
 if __name__ == "__main__":
     main()
